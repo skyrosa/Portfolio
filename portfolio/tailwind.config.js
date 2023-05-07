c214a214c62031e22aef693e622e40fa3fdc51cb
Scarlet Rosa <likedyourbody@gmail.com>
2023-05-07 16:30:37 +0200

    react install

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        white: '#FFFAFA',
+        yellow: '#FFF9F2',
+        pink: '#FFCCC4',
+        red: '#CE0000',
+        coffee: '#910000',
+        brown: '#470000',
+        navy: '#0D0060',
+      },
+      fontFamily: {
+        h1: ['Aldrich', 'sans-serif'],
+        h2: ['VT323', 'monospace'],
+      },
+    },
+  }
+}
